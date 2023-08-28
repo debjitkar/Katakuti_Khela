b5b9567c912173e74c1195855675640ec594743f
debjitkar <114505531+debjitkar@users.noreply.github.com>
2023-08-28 22:20:38 +0530

    Katakuti Khela (Tic Tac Toe).py

--- katakuti khela.py
+++ katakuti khela.py
@@ -1,86 +1,95 @@
-# for displaying the tic tac toe board
-def board(a):
-    print("",a[1]," │",a[2]," │ ",a[3]," ")
-    print("────┼────┼────")
-    print("",a[4]," │",a[5]," │ ",a[6]," ")
-    print("────┼────┼────")
-    print("",a[7]," │",a[8]," │ ",a[9]," ")
-    
-# for displaying the instruction of game
-def instructions():
-    print("\n----------- WELCOME TO TIC TAC TOE ------------\n\n")
-    board(pos)
-    print()
-    
-    players[0] = input("Enter the name of the Player 1 : ")
-    players[1] = input("Enter the name of the Player 2 : ")
-    
-    print("\n-------- Instructions ---------")
-    print("=>>",players[0],"you will be using X")
-    print("=>>",players[1],"you will be using O")
-    print("=>> Turn starts from",players[0])
-    print("=>> Potisions are :-")
-    print("  1 │  2 │ 3  ")
-    print("────┼────┼────")
-    print("  4 │ 5  │ 6  ")
-    print("────┼────┼────")
-    print("  7 │ 8  │ 9  ")
-    print("=>> press S to start the game")
-    flag = input()    
-    return flag
+# Printing the board
 
-# for starting the game
-def startgame():
-    turn = 0
-    for i in range(9):
-        if turn % 2 == 0:
-            print("\nThis is ",players[0],"\'s  turn")
-            p = int(input("Please enter the postion : "))
-            v = 'X'
-            pos[p] = v
-            board(pos)
-            winner = checkwin(v)
-            if winner == "nobody":
-                turn = 1
-                continue
-            else:
-                print("\n\n",players[0],"won the game!")
+def print_board(board):
+    for row in board:
+        print(" | ".join(row))
+        print("-" * 9)
+
+# Checking for the winner by iterating through each row of the board. 
+
+def check_winner(board, player):
+    for row in board:
+        if all(cell == player for cell in row):
+            return True
+
+    '''
+    Checking for the winner in the columns.
+    iterating through each column (0, 1, and 2)
+    and checking if all cells in that column are owned by the current player
+    '''
+    
+    for col in range(3):
+        if all(board[row][col] == player for row in range(3)):
+            return True
+
+    '''
+    Checking for the winner in the diagonals.
+    The first condition will check the diagonal from top-left to bottom-right,
+    and the second condition will check the diagonal from top-right to bottom-left
+    '''
+
+    if all(board[i][i] == player for i in range(3)) or all(board[i][2 - i] == player for i in range(3)):
+        return True
+    # if no conditions are met then returning false indiactes the current player did not win
+
+    return False
+
+# Checking if all the cells in the board is occupied or not
+
+def is_board_full(board):
+    return all(cell != " " for row in board for cell in row)
+
+'''
+The game begins in main function.
+current player is assigned to X
+3x3 empty board is created
+'''
+
+def main():
+    current_player = "X"
+    board = [[" " for _ in range(3)] for _ in range(3)]
+
+    '''
+    Printing the current state of the board
+    asking the current player to input their desired row and column
+    '''
+
+    while True:
+        print_board(board)
+        row = int(input(f"Player {current_player}, enter row (0-2): "))
+        col = int(input(f"Player {current_player}, enter column (0-2): "))
+
+        '''
+        checking if the chosen cell is empty
+        if it is empty then the current player's symbol is placed in the chosen cell
+        '''
+
+        if board[row][col] == " ":
+            board[row][col] = current_player
+
+            '''
+            checking if the current player has won
+            if true then the board is printed & the winner is anounced
+            breaking the loop
+            '''
+
+            if check_winner(board, current_player):
+                print_board(board)
+                print(f"Player {current_player} wins!")
                 break
+
+            #If there's no winner but the board is full, printing the board and announcing the draw before breaking out of the loop.
+
+            elif is_board_full(board):
+                print_board(board)
+                print("It's a draw!")
+                break
+
+            current_player = "O" if current_player == "X" else "X"
         else:
-            print("\nThis is ",players[1],"\'s  turn")
-            p = int(input("Please enter the postion : "))
-            v = 'O'
-            pos[p] = v
-            board(pos)
-            winner = checkwin(v)
-            if winner == "nobody":
-                turn = 0
-                continue
-            else:
-                print("\n\n",players[1],"won the game!")    
-                break
-    else:
-        print("\n\nGame Tied!")
-        
-# checking the winner 
-def checkwin(v):
-    for i in winning_conditions:
-        if (pos[i[0]], pos[i[1]], pos[i[2]]) == (v,v,v):
-            winner = players[0]
-            break
-        elif (pos[i[0]], pos[i[1]], pos[i[2]]) == (v,v,v):
-            winner = players[1]
-            break
-    else:
-        winner = "nobody"
-    return winner
+            print("That cell is already taken. Try again.")
 
- 
-pos = ['',' ',' ',' ',' ',' ',' ',' ',' ',' ']
-players = ['','']
-winning_conditions = [(1,2,3),(4,5,6),(7,8,9),(1,4,7),(2,5,8),(3,6,9),(1,5,9),(3,5,7)]
-flag = instructions()
-if flag.lower() == 's':
-    startgame()
-else:
-    print("Invalid Entry")
+# Running the main function, ensuring that the game starts when the python file is run.
+
+if __name__ == "__main__":
+    main()
